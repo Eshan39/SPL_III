64d8552d028f2c6db4bb557b33711fdd8c0ae40a
Eshan39 <35795854+Eshan39@users.noreply.github.com>
2021-03-31 17:22:25 +0600

    redundant statements detects done

---
+++ TestMinimization/src/kajKoro/RedundantClass.java
@@ -0,0 +1,18 @@
+package kajKoro;
+
+import java.util.ArrayList;
+
+public class RedundantClass {
+
+	ArrayList<String> stmts = new ArrayList<String>();
+    String name = null;
+    
+	public  RedundantClass(String name,ArrayList<String> stmts) {
+		this.name=name;
+		this.stmts=(ArrayList<String>) stmts.clone();	
+		
+	}
+		
+}
+
+
